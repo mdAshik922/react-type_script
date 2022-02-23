1cfe0d4ca81d845242665b4744e5fc49df1d3254
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-23 18:55:35 +0600

    commit

---
+++ src/Component/List/List.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List = ({items, onClick}  {
+    items: string[],
+    onClick: () => void;
+}) => {
+    return (
+        <div>
+            List
+        </div>
+    );
+};
+
+export default List;
