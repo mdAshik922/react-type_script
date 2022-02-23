90c82e6d4e6a97e22ba6fd2c687697249b5d3eb0
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-23 20:01:45 +0600

    commit

--- src/Component/Lists.tsx
+++ src/Component/Lists.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Lists = () => {
     const item: string[] = ["Tammim", "Ashik"];
-const onClick = (text: string)=> alert()
+const onClick = (text: string): void=> alert()
     return (
         <div>
             <List items={items} />{""}
