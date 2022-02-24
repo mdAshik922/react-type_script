bef4cba083fbbce7c07ac7c60ed777faeb2b9579
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-24 08:18:10 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Lists from './Component/Lists';
 
 function App() {
+
+
+  const [todos, dispatch] = useReducer(reducer, initialState));
+  
   return (
+   
     <div className="App">
      <Lists/>
     </div>
