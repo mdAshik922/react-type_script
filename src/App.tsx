7a5984112d4dd3280a7ea9a5215e60405c8c791d
mdAshik922 <md.ashik0005723@gmail.com>
2022-02-24 08:55:51 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useReducer, useRef, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Lists from './Component/Lists';
+import { idText } from "typescript";
 
 
 interface Todo{
@@ -9,14 +10,25 @@ id: number,
 text: string,
 }
 
-type ActionType =   | { type: "ADD"; text: string }
+type ActionType =  
+ | { type: "ADD"; text: string }
 | { type: "REMOVE"; id: number };
 
 
 function App() {
   function reducer(state: Todo[], action: ActionType) {
 switch (action.type) {
- 
+ case"ADD":
+ return{
+   ...state,
+   {
+    id: state.length,
+    text: action.text,
+  },
+  case "REMOVE":
+    return state.filter(({ id }) => id !== action.id);
+ }
+
 }
   }
 
